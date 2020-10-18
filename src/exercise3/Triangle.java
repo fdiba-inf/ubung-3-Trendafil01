fe10cb754a343eb3a0f9c4a02ba9597864d18038
Trendafil01 <trendafil.trendafilov@fdiba.tu-sofia.bg>
2020-10-18 19:33:35 +0000

    ex3

--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -4,38 +4,35 @@ import java.util.Scanner;
 
 public class Triangle {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    double a = input.nextDouble();
+    double b = input.nextDouble();
+    double c = input.nextDouble();
+    double s = 0;
+    double area = 0;
 
-        double a;
-        double b;
-        double c;
-        do {
-          System.out.println("Enter a value for a: ");
-          a = input.nextDouble();
-          System.out.println("Enter a value for b: ");
-          b = input.nextDouble();
-          System.out.println("Enter a value for c: ");
-          c = input.nextDouble();
-          boolean fiki = a > 0 && b > 0 && c > 0 && b+c>a && a+c>b && a+b>c;
-          if (fiki){
-            double s = (a + b + c) / 2.0;
-            double Area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-            System.out.println("Area: " + Area);
-            if (a == b && b == c ){
-             System.out.println("Triangle: equilateral" );
-            } else if ( a == b || a == c || b == c){
-             System.out.println("Triangle: isosceles");
-            } else  {
-              System.out.println("Triangle: scalene");
-            }
-          } else {
-            System.out.println("Values are not correct! ");
-            
-          }
-             break; 
-        } while ( a > 0 || b > 0 || c > 0);
+    do {
+      if (a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b) {
 
-    }
+        s = (a + b + c) / 2.0;
+        area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        System.out.println("Area: " + area);
+        if (a == b && b == c) {
+          System.out.println("Triangle: equilateral");
 
+        } else if (a == b || a == c || b == c) {
+          System.out.println("Triangle: isosceles");
+        } else if (a != b && b != c && a != c) {
+          System.out.println("Triangle: scalene");
+        }
+      } else {
+        System.out.println("Values are not correct!");
+      }
+      a = input.nextDouble();
+      b = input.nextDouble();
+      c = input.nextDouble();
+
+    } while (a > 0 && b > 0 && c > 0);
+  }
 }
